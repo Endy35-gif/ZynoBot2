fad1731bf105f0c896e7028ec9eed76b4d2a12c0
Endy35-gif <dghfhjvhiyfbjuf@gmail.com>
2026-02-21 12:30:09 +0100

    Create based.js

---
+++ based.js
@@ -0,0 +1,72 @@
+import './config.js';
+import { createRequire } from 'module';
+import path, { join } from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
+import { platform } from 'process';
+import fs, { readdirSync, statSync, unlinkSync, existsSync, readFileSync, mkdirSync, rmSync, watch } from 'fs';
+import yargs from 'yargs';
+import { spawn } from 'child_process';
+import lodash from 'lodash';
+import chalk from 'chalk';
+import { tmpdir } from 'os';
+import { format } from 'util';
+import pino from 'pino';
+import { makeWASocket, protoType, serialize } from './lib/simple.js';
+import { Low, JSONFile } from 'lowdb';
+import NodeCache from 'node-cache';
+import { ripristinaTimer } from './plugins/gp-configgruppo.js';
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1';
+
+const DisconnectReason = {
+    connectionClosed: 428,
+    connectionLost: 408,
+    connectionReplaced: 440,
+    timedOut: 408,
+    loggedOut: 401,
+    badSession: 500,
+    restartRequired: 515,
+    multideviceMismatch: 411,
+    forbidden: 403,
+    unavailableService: 503
+};
+const { useMultiFileAuthState, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser, getPerformanceConfig, setPerformanceConfig, Logger, makeInMemoryStore } = await import('@realvare/based');
+const { chain } = lodash;
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
+protoType();
+serialize();
+global.isLogoPrinted = false;
+global.qrGenerated = false;
+global.connectionMessagesPrinted = {};
+let methodCodeQR = process.argv.includes("qr");
+let methodCode = process.argv.includes("code");
+let MethodMobile = process.argv.includes("mobile");
+let phoneNumber = global.botNumberCode;
+
+function redefineConsoleMethod(methodName, filterStrings) {
+    const originalConsoleMethod = console[methodName];
+    console[methodName] = function () {
+        const message = arguments[0];
+        if (typeof message === 'string' && filterStrings.some(filterString => message.includes(atob(filterString)))) {
+            arguments[0] = "";
+        }
+        originalConsoleMethod.apply(console, arguments);
+    };
+}
+
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') {
+    return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString();
+};
+
+global.__dirname = function dirname(pathURL) {
+    return path.dirname(global.__filename(pathURL, true));
+};
+
+global.__require = function require(dir = import.meta.url) {
+    return createRequire(dir);
+};
+
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '');
+global.timestamp = { start: new Date };
+const __dirname = global.__dirname(import.meta.url);
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
