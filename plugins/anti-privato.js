3268949993dd991ebe0e3d304c449a0aa742c61a
Endy35-gif <dghfhjvhiyfbjuf@gmail.com>
2026-02-21 03:33:20 +0100

    Update anti-privato.js

--- plugins/anti-privato.js
+++ plugins/anti-privato.js
@@ -3,8 +3,8 @@ export async function before(m, { isOwner, isRowner, isMods}) {
     if (m.isGroup) return !1;
     if (!m.message) return !0;
     if (m.text.includes('sasso') || m.text.includes('carta') || m.text.includes('forbici')) return !0; 
-    const varebot = global.db.data.settings[this.user.jid] || {};
-    if (varebot.antiprivato && !isOwner && !isRowner && !isMods) {
+    const zynobot = global.db.data.settings[this.user.jid] || {};
+    if (zynobot.antiprivato && !isOwner && !isRowner && !isMods) {
       await this.updateBlockStatus(m.chat, 'block');
     }
     return !1;
